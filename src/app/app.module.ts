f6fd8269e8d418959d4488637d22d90c912bf8fe
Jean Delaunay <jean.delaunay@epitech.eu>
2021-05-31 10:00:28 +0200

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './team/team.component';
+import { ProjectComponent } from './project/project.component';
+import { LightpollutionComponent } from './lightpollution/lightpollution.component';
+import {RouterModule, Routes} from "@angular/router";
+
+export const ROUTES : Routes = [
+  {path:'', component: HomeComponent},
+  {path:'home', component: HomeComponent},
+  {path:'team', component: TeamComponent},
+  {path:'project', component: ProjectComponent},
+  {path:'lightPollution', component: LightpollutionComponent},
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TeamComponent,
+    ProjectComponent,
+    LightpollutionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/lightpollution/lightpollution.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lightpollution',
+  templateUrl: './lightpollution.component.html',
+  styleUrls: ['./lightpollution.component.scss']
+})
+export class LightpollutionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
